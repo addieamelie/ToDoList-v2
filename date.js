78d893346b4e9060dcd1df800862e29e5174d283
Adrianna <addie.nosal@gmail.com>
2020-01-07 16:16:18 +0000

    add and use multiple functions from module

--- date.js
+++ date.js
@@ -1,8 +1,13 @@
 // Adding new module
 
-module.exports.getDate = getDate;
+//1.Get rid of function name - make anon function
+//2.Store it in a variable
+//3.Shorten these 2 lines so one thing equals another
+//   module.exports.getDate = getDate;
+//   var getDate = function() {
+//4. Use arrow function
 
-function getDate() {
+module.exports.getDate = () => {
   let today = new Date();
 
   var options = {
@@ -10,22 +15,18 @@ function getDate() {
     weekday: "long",
     month: "long"
   };
-
-  let day = today.toLocaleDateString("en-US", options);
-  return day;
-}
+  return today.toLocaleDateString("en-US", options);
+};
 
 //export another function from module
-module.exports.getDay = getDay;
 
-function getDay() {
-    let today = new Date();
-  
-    var options = {
-      weekday: "long"
-    };
-  
-    let day = today.toLocaleDateString("en-US", options);
-    return day;
-  }
-  console.log(module.exports);
+module.exports.getDay = () => {
+  let today = new Date();
+
+  var options = {
+    weekday: "long"
+  };
+
+  return today.toLocaleDateString("en-US", options);
+};
+console.log(module.exports);
