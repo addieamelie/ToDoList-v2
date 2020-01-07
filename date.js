024186f834af3df3fd0941939560c75660ba684e
Adrianna <addie.nosal@gmail.com>
2020-01-07 15:53:49 +0000

    add header footer leyot, add about page

---
+++ date.js
@@ -0,0 +1,13 @@
+// Adding new module
+function getDate() {
+  let today = new Date();
+
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+  };
+
+  let day = today.toLocaleDateString("en-US", options);
+  return day;
+}
