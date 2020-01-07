3aca6fab297dbe39e7e6c3889cc8579963d544b4
Adrianna <addie.nosal@gmail.com>
2020-01-07 15:17:16 +0000

    add css styling

--- app.js
+++ app.js
@@ -5,28 +5,28 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = [];
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs"); //below the express one cause uses app
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  
   var today = new Date();
-  
+
   var options = {
-    weekday: "long", 
+    weekday: "long",
     day: "numeric",
     month: "long"
   };
 
   var day = today.toLocaleDateString("en-US", options);
-   //list.ejs has to be in views
-  res.render("list", {kindOfDay: day, newListItems: items}); //some ppl use same name as in ejs file but it's easier to differentiate
+  //list.ejs has to be in views
+  res.render("list", { kindOfDay: day, newListItems: items }); //some ppl use same name as in ejs file but it's easier to differentiate
 });
 
-app.post("/", (req,res) => {
+app.post("/", (req, res) => {
   var item = req.body.newItem;
   items.push(item);
   res.redirect("/"); //redirect to home route
