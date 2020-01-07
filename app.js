4e85801ff38a02ba281487afd0eea394987e4f78
Adrianna <addie.nosal@gmail.com>
2020-01-07 16:57:18 +0000

    style fix, server fix

--- app.js
+++ app.js
@@ -40,6 +40,6 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000.");
 });
