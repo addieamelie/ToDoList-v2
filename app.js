8d818aaecd3524c6407267118f1a1cb6c071ba01
Adrianna <addie.nosal@gmail.com>
2020-01-08 02:01:06 +0000

    add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const date = require(__dirname + "/date.js"); // ALWAYS REMEMBER ////// SLASH
 
 const app = express();
@@ -15,6 +16,11 @@ app.set("view engine", "ejs"); //below the express one cause uses app
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 app.get("/", (req, res) => {
   const day = date.getDate(); //can change to date.getDay()
